687591c9bd5fbe6b7efc3428e8f3e46eb0aea978
aiswlz <153474803+aiswlz@users.noreply.github.com>
2025-05-30 15:35:12 +0500

    Update user.go

--- user_service/internal/domain/user.go
+++ user_service/internal/domain/user.go
@@ -3,8 +3,8 @@ package domain
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Name     string             `bson:"name"`
-	Email    string             `bson:"email"`
-	Password string             `bson:"password"`
+	ID       primitive.ObjectID 
+	Name     string             
+	Email    string            
+	Password string             
 }
